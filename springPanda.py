855ab921cd29427ee08ee5aa4ae776114a8f74e2
trocprgm <trocprgm@protonmail.com>
2023-12-07 10:53:39 -0800

    deleted:    .~lock.Run1.csv# 	new file:   .~lock.Run5.csv# 	modified:   Run5.csv 	deleted:    a.out 	deleted:    posi.txt 	modified:   springPanda.py

--- springPanda.py
+++ springPanda.py
@@ -1,34 +1,21 @@
-import math
-import csv
 import pandas as pd
+import sys
+
+filename = sys.argv[1]
+selection = sys.argv[2]
+run = pd.read_csv(filename, header=None)
+run = (run.rename(columns={0:'Time',1:'Pos'})
+         .assign(isPeak=False,
+                 isPit=False,
+                 isExt=False,
+                 isDupe=False,
+                 Period=0,
+                 CalPos=0)
+      )
 
 
-
-def run(int):
-    match int:
-        case 1:
-            run = pd.read_csv("Run1.csv", header=None)
-        case 2:
-            run = pd.read_csv("Run2.csv", header=None)
-        case 3:
-            run = pd.read_csv("Run3.csv", header=None)
-        case 4:
-            run = pd.read_csv("Run4.csv", header=None)
-        case 5:
-            run = pd.read_csv("Run5.csv", header=None)
-    return (run.rename(columns={0:'Time',1:'Pos'})
-               .assign(isPeak=False,
-                       isPit=False,
-                       isExt=False,
-                       isDupe=False,
-                       Period=0,
-                       CalPos=0)
-            )
-
-wave = run(4)
 pd.set_option('display.max_rows', None,
               'display.precision', 6)
-
 
 def peri(davey):
     lasExt = 0
@@ -53,38 +40,20 @@ def peri(davey):
                     lasExt = davey.at[i,'Time']
             if davey.at[i,'Pos'] ==  davey.at[(i-1),'Pos'] or davey.at[i,'Pos'] == davey.at[(i+1),'Pos'] :
                 davey.at[i,'isDupe'] = True
-    jsext = wave[wave.Period >0]
+    jsext = davey[davey.Period >0]
     MeanPos = jsext.mean()
     Calcoe = MeanPos['Pos']
-    #print(davey.append(Caos=davey['Pos']-Calcoe))
     davey['CalPos'] = davey['Pos'] - Calcoe    
-    #ip = 0            
-    #for i in wave.index[wave['isExt']]:
-    #    ind = wave.index[wave['isExt']]
-    #    ind.reindex
-    #    davey.at[i,'Period'] = 2 * (davey.at[i,'Time'] - ip)
-    #    ip += 1
-
     return davey
 
-wave=peri(wave)
-
-print(wave)
-
-
-
-
-
-
-
-
-
-
+#print("Mean Period: " + str(round(Calcoe,4)))
 def absdif(num1, num2):
     return abs(abs(num1) - abs(num2))
 
 def tperiod(mass,k):
     return round((2 * math.pi * math.sqrt(mass /k )),6)
+
+
 
 
 mass = 0
@@ -94,3 +63,16 @@ deff1 = 0
 deff2 = 0
 k = abs( absdif(deff1,deff2) / absdif(delx1,delx2))
 
+def out(int):
+    wave=peri(run)
+    jsext = wave[wave.Period >0]
+    MeanSel = jsext.mean()
+    match int:
+        case 1:
+            print(wave)
+        case 2:
+            print(jsext[['Time','Pos','isDupe', 'Period','CalPos']])
+        case 3:
+            print(MeanSel[['Time','Pos','Period']])
+
+out(int(selection))
